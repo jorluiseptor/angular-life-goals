67d160a0a4c3ba98ba983cfb10df821d1a5ea1d6
Jorge Sepulveda <jorluiseptor@gmail.com>
2017-11-24 13:46:32 -0500

    Add routes. Training video is up to 44:53

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }  from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  goalId: number; 
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    //gets the value from the route (URL) parameter
+    this.route.params.subscribe(
+      res => this.goalId= res.id
+    );
+  }
 
   ngOnInit() {
+  }
+
+  sendMeHome(){
+    this.router.navigate(['']); //the home path is blank
   }
 
 }
